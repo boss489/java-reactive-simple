f5f65b557260692fa968f98935deabebbdefdcd5
bosu.kim01 <bosu.kim01@lotte.net>
2025-01-10 06:34:09 +0900

    백프레셔 전략

---
+++ src/main/java/com/reactive/common/AbstractHttpClient.java
@@ -0,0 +1,15 @@
+package com.reactive.common;
+
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.LoopResources;
+
+public abstract class AbstractHttpClient {
+
+    private static final String BASE_URL = "http://localhost:7070";
+    protected final HttpClient httpClient;
+
+    public AbstractHttpClient() {
+        var loopResources = LoopResources.create("vins", 1, true); //event루프 그룹을 내부적으로 생성
+        this.httpClient = HttpClient.create().runOn(loopResources).baseUrl(BASE_URL);
+    }
+}
